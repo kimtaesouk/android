d9663220be155dfa00e7ab3f47a386f387fd1836
kimts <kimtsnaeun@naver.com>
2024-09-11 17:48:23 +0900

    commit

---
+++ app/src/main/java/com/example/newproject/Chat/DetailProfile_to_Chat.java
@@ -0,0 +1,16 @@
+package com.example.newproject.Chat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.newproject.R;
+
+public class DetailProfile_to_Chat extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_profile_to_chat);
+    }
+}
